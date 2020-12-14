23498e595f4be2258e5ac97f504363f6dfd7c7e3
Brian Pippin <brian.pippin@caci.com>
2020-12-14 15:31:08 -0500

    Design for tabs and saving and loading saved queries

---
+++ mcs_docker_setup/analysis-ui/src/components/App/loadQuery.jsx
@@ -0,0 +1,124 @@
+import React, {useState} from 'react';
+import Modal from 'react-bootstrap/Modal';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const getSavedQueriesName = "getSavedQueries";
+
+const LOAD_SAVED_QUERIES = gql`
+    query getSavedQueries{
+        getSavedQueries {
+            name
+            user
+            queryObj
+            description
+            createdDate
+        }
+    }`;
+
+function LoadQueryModal({show, onHide, currentUser, loadQueryHandler}) {
+    const [activeTab, setActiveTab] = useState("load_my_queries");
+
+    const loadQuery = (query) => {
+        loadQueryHandler(query);
+        onHide();
+    }
+
+    const closeModal = () => {
+        onHide();
+    }
+
+    return (
+        <Modal show={show} onHide={closeModal} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter-load">Load Query</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Query query={LOAD_SAVED_QUERIES} fetchPolicy={'network-only'}>
+                {
+                    ({ loading, error, data }) => {
+                        if (loading) return <div>No comments yet</div> 
+                        if (error) return <div>Error</div>
+                        
+                        let queries = data[getSavedQueriesName];
+                        if(activeTab === 'load_my_queries') {
+                            queries = queries.filter(query => query.user.id === currentUser.id)
+                        }
+                        queries.sort(function(a, b){return b.createdDate - a.createdDate});
+
+                        return (
+                            <div className="load-query-contents">
+                                <div className="load-query-nav">
+                                    <ul className="nav nav-tabs">
+                                        <li className="nav-item">
+                                            <button id={'load_my_queries'} className={'load_my_queries' === activeTab ? 'nav-link active' : 'nav-link'} onClick={() => setActiveTab('load_my_queries')}>My Queries</button>
+                                        </li>
+                                        <li className="nav-item">
+                                            <button id={'load_all_queries'} className={'load_all_queries' === activeTab ? 'nav-link active' : 'nav-link'} onClick={() => setActiveTab('load_all_queries')}>All Queries</button>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="query-tab-contents saved-query-table">
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Date</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                                {activeTab === 'load_all_queries' &&
+                                                    <th>User</th>
+                                                }
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {queries.map((query, key) => 
+                                                <tr key={'saved_query_row_' + key}>
+                                                    <td>{(new Date(query.createdDate)).toLocaleString()}</td>
+                                                    <td>{query.name}</td>
+                                                    <td>{query.description}</td>
+                                                    {activeTab === 'load_all_queries' &&
+                                                        <td>{query.user.username}</td>
+                                                    }
+                                                    <td>
+                                                        <button type="button" className="btn btn-secondary btn-sm" onClick={() => loadQuery(query)}>Load</button>
+                                                    </td>
+                                                </tr>
+                                            )}
+                                        </tbody>
+                                    </table>
+                                </div>   
+                            </div>
+                        )
+                    }
+                }
+                </Query>
+            </Modal.Body>
+        </Modal>
+    );
+}
+
+function LoadQuery ({currentUser, loadQueryHandler}) {
+
+    const [modalShow, setModalShow] = React.useState(false);
+
+    return (
+        <>
+            <a href="#loadQueryLink" onClick={() => setModalShow(true)} className="icon-link">
+                <span className="material-icons icon-margin-right">
+                    folder_open
+                </span>
+            </a>
+
+            <LoadQueryModal
+                show = {modalShow}
+                onHide = {() => setModalShow(false)}
+                currentUser = {currentUser}
+                loadQueryHandler = {loadQueryHandler}
+            />
+        </>
+    );
+    
+}
+
+export default LoadQuery;
---
+++ mcs_docker_setup/analysis-ui/src/components/App/saveQuery.jsx
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import {useMutation} from 'react-apollo';
+import gql from 'graphql-tag';
+
+const SAVE_QUERY = gql`
+    mutation saveQuery($user: JSON!, $queryObj: JSON!, $name: String!, $description: String!, $createdDate: Float!){
+        saveQuery(user: $user, queryObj: $queryObj, name: $name, description: $description, createdDate: $createdDate) {
+            name
+        }
+  }`;
+
+function SaveQueryModal({show, onHide, queryObj, currentUser, queryId, updateQueryNameHandler}) {
+    const [queryName, setQueryName] = useState("");
+    const [queryDesc, setQueryDesc] = useState("");
+    const [saveQuerryCall, { data }] = useMutation(SAVE_QUERY);
+
+    const resetSaveForm = () => {
+        setQueryName("");
+        setQueryDesc("");
+    }
+
+    const saveQuery = () => {
+        saveQuerryCall({ variables: { 
+            user: currentUser,
+            queryObj: queryObj,
+            name: queryName,
+            description: queryDesc,
+            createdDate: (new Date()).valueOf()
+        } });
+        updateQueryNameHandler(queryId, queryName);
+        resetSaveForm();
+        onHide();
+    };
+
+    const closeModal = () => {
+        resetSaveForm();
+        onHide();
+    }
+
+    return (
+        <Modal show={show} onHide={closeModal} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">Save Query</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <form>
+                    <div className="form-group">
+                        <div className="input-login-header">Name</div>
+                        <input className="form-control form-control-lg" placeholder="Query Name" type="text" value={queryName} onChange={e => setQueryName(e.target.value)}/>
+                    </div>
+                    <div className="form-group">
+                        <div className="input-login-header">Description</div>
+                        <input className="form-control form-control-lg" placeholder="Query Description" type="text" value={queryDesc} onChange={e => setQueryDesc(e.target.value)}/>
+                    </div>
+                </form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={closeModal}>Cancel</Button>
+                <Button variant="primary" onClick={saveQuery}>Save Query</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+function SaveQuery ({queryObj, currentUser, queryId, updateQueryNameHandler}) {
+
+    const [modalShow, setModalShow] = React.useState(false);
+
+    return (
+        <>
+            <a href="#saveQueryLink" onClick={() => setModalShow(true)} className="icon-link">
+                <span className="material-icons icon-margin-left">
+                    save
+                </span>
+                <span className="icon-link-text">Save</span>
+            </a>
+
+            <SaveQueryModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                queryObj={queryObj}
+                currentUser={currentUser}
+                queryId={queryId}
+                updateQueryNameHandler={updateQueryNameHandler}
+            />
+        </>
+    );
+    
+}
+
+export default SaveQuery;
