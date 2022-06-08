d7b878d30bfb8c2a7ad27b8c25b100859be1a8b1
Rachel Artiss <rachel.artiss@caci.com>
2022-06-07 20:09:27 -0400

    Fixed per PR comment.

--- analysis-ui/src/components/TestOverview/evalNavItem.jsx
+++ analysis-ui/src/components/TestOverview/evalNavItem.jsx
@@ -62,7 +62,7 @@ class EvalNavItem extends React.Component {
                     // Remove Evaluation 2 Results because this page is designed to show hypercube results
                     //    and we did not have a hypercube design for Evaluation 2
 
-                    evalOptions = evalOptions.filter(element => element.label !== "eval_2_results");
+                    evalOptions = evalOptions.filter(element => element.label !== "Evaluation 2 Results");
 
                     return (
                         <List className="nav-list" component="nav" aria-label="secondary mailbox folder">
