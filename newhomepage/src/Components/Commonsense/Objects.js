1b362afb42463794cadd9bb0ea7bf06a1a22b570
Le <phi.le_cn@cacicorenet.com>
2023-12-13 14:11:08 -0500

    today's updates

--- newhomepage/src/Components/Commonsense/Objects.js
+++ newhomepage/src/Components/Commonsense/Objects.js
@@ -6,7 +6,7 @@ function Objects() {
         <div className="main-text">
             <h2>Commonsense in the Objects Domain</h2>
             <p>
-                Tasks were designed to assess AI understanding of common sense about objects. Many scientific studies have examined infants' understanding that objects continue to exist when they can no longer be seen (object permanence), the fact that objects are solid, subject to gravity, can be tracked as they move through space, among other things. Young children also develop a commonsense understanding of tool use, interactions between objects (i.e., collisions), and the numerical properties of sets of objects. As part of the MCS program, tasks were developed to assess AI common sense in these areas, using as inspiration experimental work conducted with infants and young children.
+            Tasks were designed to assess AI systems' common sense about objects. Many scientific studies have examined infants' understanding that objects continue to exist when they can no longer be seen (object permanence), and the facts that objects are solid, subject to gravity, can be tracked as they move through space, among other things. Young children also develop a commonsense understanding of tool use, interactions between objects (i.e., collisions), and the numerical properties of sets of objects. As part of the MCS program, tasks were developed to assess AI common sense in these areas, using as inspiration experimental work conducted with infants and young children.
             </p>
 
             <div className="float-left-50">
@@ -23,17 +23,17 @@ function Objects() {
             <div className="float-left-50">
                 <div>
                     <h3>Interactive Object Tasks:</h3>
-                    <li><a href="/CommonsenseDomain/Task?TaskId=Artithmetic">Addition and Subtraction</a></li>
+                    <li><a href="/CommonsenseDomain/Task?TaskId=Arithmetic">Addition and Subtraction</a></li>
                     <li><a href="/CommonsenseDomain/Task?TaskId=AsymmetricToolUse">Asymmetric Tool Use</a></li>
                     <li><a href="/CommonsenseDomain/Task?TaskId=InteractiveObjectPermanence">Interactive Object Permanence</a></li>
                     <li><a href="/CommonsenseDomain/Task?TaskId=SupportRelations">Interactive Support Relations</a></li>
                     <li><a href="/CommonsenseDomain/Task?TaskId=MovingTargetPrediction">Moving Target Prediction</a></li>
                     <li><a href="/CommonsenseDomain/Task?TaskId=NumberComparison">Number Comparison</a></li>
                     <li><a href="/CommonsenseDomain/Task?TaskId=OccludedTrajectory">Occluded Trajectory</a></li>
-                    <li><a href="/CommonsenseDomain/Task?TaskId=Trajectory">Predict Post-Collision Trajectory</a></li>
+                    <li><a href="/CommonsenseDomain/Task?TaskId=InteractiveCollision">Predict Post-Collision Trajectory</a></li>
                     <li><a href="/CommonsenseDomain/Task?TaskId=SecondaryToolUse">Secondary Tool Use</a></li>
                     <li><a href="/CommonsenseDomain/Task?TaskId=Solidity">Solidity</a></li>
-                    <li><a href="/CommonsenseDomain/Task?TaskId=CollisionsToolUse">Tool Use</a></li>
+                    <li><a href="/CommonsenseDomain/Task?TaskId=ToolUse">Tool Use</a></li>
                 </div>
             </div>
         </div>
