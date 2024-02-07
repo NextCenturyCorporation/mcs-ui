13b4ee0c361bebf6a31f4e3de6344565e5e58ab6
Le <phi.le_cn@cacicorenet.com>
2024-02-07 14:49:32 -0500

    pushing latest updates

--- newhomepage/src/Components/TechnicalInfo/EvalAnalysisUI.js
+++ newhomepage/src/Components/TechnicalInfo/EvalAnalysisUI.js
@@ -16,8 +16,84 @@ function EvalAnalysisUI() {
       <h3>Scene Analysis</h3>
       <p></p>
 
+      <h3>EOD-EXO Simulation</h3>
+      <h5>Map Size:</h5>
+      <input placeholder='In Meters'/>
+      <h5>Simulation from Address:</h5>
+      <input placeholder='Enter Address'/> <button>Generate</button>
+      <h5>Random Simulation Environment:</h5>
+      <select placeholder='Choose Environment'>
+        <option>Urban</option>
+        <option>Woodland/Jungle</option>
+        <option>Mountain</option>
+        <option>Desert</option>
+      </select> <button>Generate</button>
+
+
+
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+      <h5>Improvised explosive device (IED) :</h5>
+      <select placeholder='Choose Environment'>
+        <option>Pressure/Pressure Release</option>
+        <option>Trip Wire</option>
+        <option>Sensors</option>
+        <option>Radio Controlled</option>
+        <option>Command Wire</option>
+      </select>
+
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+
+      <h5>Explosive Capacity:</h5>
+      <select placeholder='Choose Environment'>
+        <option>1 lb</option>
+        <option>5 lb</option>
+        <option>10 lb</option>
+        <option>20 lb</option>
+        <option>50 lb</option>
+      </select>
+
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+
+      <h5>Date:</h5>
+      <input type="date" id="date" name="date"/><br/>
+      <h5>Time:</h5>
+      <input type="time" id="time" name="time"/>
+      <h5>Lunar Phase:</h5>
+      <select placeholder='Lunar Phase'>
+        <option>Default (Date/Time/Geolocation)</option>
+        <option>First Quarter</option>
+        <option>Full</option>
+        <option>Third Quarter</option>
+        <option>New</option>
+      </select>
+      <h5>Weather:</h5>
+      <select placeholder='Choose Environment'>
+        <option>Default (Date/Time/Geolocation)</option>
+        <option>Clear</option>
+        <option>Rain</option>
+        <option>Snow</option>
+      </select>
+
+
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+
+      <h5>Ground based ordinance:</h5>
+      <select>
+        <option>Grenades</option>
+        <option>Landmines</option>
+        <option>Projectiles</option>
+      </select>
+
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+
 
     </div>
+
   );
 }
 
